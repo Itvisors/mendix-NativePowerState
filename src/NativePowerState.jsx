8d67d7019f067026fb1a47012c3e1479ab019492
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2022-11-03 07:07:26 +0100

    Allow enum attributes too. Code cleanup

--- src/NativePowerState.jsx
+++ src/NativePowerState.jsx
@@ -4,13 +4,20 @@ import { usePowerState } from "react-native-device-info";
 
 export function NativePowerState({ powerStateAttr, onPowerStateChanged }) {
     const powerState = usePowerState();
-    const [currentBatteryState, setCurrentBatteryState] = useState("");
+    const [currentBatteryState, setCurrentBatteryState] = useState(undefined);
     useEffect(() => {
-        if (!currentBatteryState || currentBatteryState !== powerState.batteryState) {
-            // console.info("Power state change, old: " + currentBatteryState + ", new: " + powerState.batteryState);
-            const newBatteryState = "" + powerState.batteryState;
+        const newBatteryState = powerState?.batteryState ? "" + powerState.batteryState : undefined;
+        if (newBatteryState !== currentBatteryState) {
+            console.info("Power state change, old: " + currentBatteryState + ", new: " + newBatteryState);
+
+            // We keep a state value too. It may take several renders before the current value of the atttribute property reflects the change.
+            // That could result in multiple calls to the action
             setCurrentBatteryState(newBatteryState);
-            powerStateAttr.setTextValue(newBatteryState);
+
+            // Update the attribute value
+            powerStateAttr.setValue(newBatteryState);
+
+            // Call the action
             if (onPowerStateChanged && onPowerStateChanged.canExecute && !onPowerStateChanged.isExecuting) {
                 onPowerStateChanged.execute();
             }
